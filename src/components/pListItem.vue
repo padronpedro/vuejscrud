728d7f8933a4c94c56120426e3c564a1c2e2563b
Pedro Padron <padronpedro@gmail.com>
2020-07-14 18:20:54 -0400

    Custom component for menu item

---
+++ src/components/pListItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-list-item link @click="goToPath(toPath)">
+    <v-list-item-action>
+      <v-icon>{{iconName}}</v-icon>
+    </v-list-item-action>
+    <v-list-item-content>
+      <v-list-item-title>{{textTitle}}</v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props: {
+    toPath: {
+      type: String,
+      required: true
+    },
+    iconName: {
+      type: String,
+      required: true
+    },
+    textTitle: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    goToPath: function (auxToPath) {
+      this.$router.push({ name: auxToPath }).catch(err => { err = null })
+      this.$eventHub.$emit('toggleDrawer')
+    }
+  }
+}
+</script>
