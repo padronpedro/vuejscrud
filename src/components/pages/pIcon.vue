da9ea430e28f15ae2a0f7116c737aa4adca444b1
Pedro Padron <padronpedro@gmail.com>
2020-07-17 17:36:43 -0400

    custom icon component with tooltip and click

---
+++ src/components/pages/pIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-tooltip bottom color="orange">
+    <template v-slot:activator="{ on }">
+      <v-btn text v-on="on" @click="btnFunction(toPath)">
+        <v-icon class="mr-2">{{iconName}}</v-icon>
+          {{ iconText }}
+        </v-btn>
+    </template>
+    <span>{{ iconText }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String
+    },
+    iconText: {
+      type: String
+    },
+    toPath: {
+      type: String
+    }
+  },
+  methods: {
+    btnFunction (path) {
+      this.$goRouter(path)
+    }
+  }
+}
+</script>
