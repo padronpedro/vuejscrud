9646bb31a6ef10db16f211a5e9e63d5951f8f25c
Pedro Padron <padronpedro@gmail.com>
2020-07-22 18:19:01 -0400

    Added new custom components

---
+++ src/components/pages/pColumn.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-col
+    :cols="cols"
+    :lg="lg"
+    :sm="sm"
+  >
+    <slot></slot>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: ['size'],
+  mounted () {
+    this.$nextTick(function () {
+      if (this.size === 'xs') {
+        this.lg = 1
+      }
+      if (this.size === 'sm') {
+        this.lg = 2
+      }
+      if (this.size === 'md') {
+        this.lg = 3
+      }
+      if (this.size === 'lg') {
+        this.lg = 6
+      }
+      if (this.size === 'xl') {
+        this.lg = 12
+      }
+    })
+  },
+  data: function () {
+    return {
+      cols: 12,
+      lg: 4,
+      sm: 6
+    }
+  }
+}
+</script>
---
+++ src/components/pages/pDivider.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-subheader class="font-weight-bold mb-n4">
+      <img
+        v-if="imageDivider"
+        :src="'/images/'+imageDivider"
+        :width="imageWidth"
+        class="mr-4"
+      />
+      {{ txtDivider }}
+      </v-subheader>
+    <v-divider />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    txtDivider: {
+      type: String,
+      required: true
+    },
+    imageDivider: {
+      type: String,
+      required: false
+    },
+    imageWidth: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
