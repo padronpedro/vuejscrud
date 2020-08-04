bbb12cb0cc5b20dcbd5102d7fd7ea4ea1477f9bb
Pedro Padron <padronpedro@gmail.com>
2020-08-04 13:15:34 -0400

    Jest: simple unit test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,67 +1,15 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <v-list dense>
-        <v-list-item link @click="goTo('Login')">
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-email</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar
-      app
-      color="indigo"
-      dark
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
-
-    <v-main>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <router-view/>
-      </v-container>
-    </v-main>
-    <v-footer
-      color="indigo"
-      app
-    >
-      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    {{ message }}
   </v-app>
 </template>
 
 <script>
 export default {
-  props: {
-    source: String
-  },
-  data: () => ({
-    drawer: false
-  }),
-  methods: {
-    goTo: function (toPath) {
-      this.$router.push({ name: toPath })
+  data () {
+    return {
+      message: 'ejemplo',
     }
-  }
+  },
 }
 </script>
