b3b9f406c0a5151bf85c36c96269e16a93a4bea9
Pedro Padron <padronpedro@gmail.com>
2020-08-04 19:10:45 -0400

    Added mock funct for testing

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,5 +1,19 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify';
+import { config } from "@vue/test-utils"
+
+// mock for i18n
+config.mocks['$t'] = (msg) => {
+  return msg
+}
+// mock for $showError
+config.mocks['$showError'] = (message, color, timeout, params) => {
+  return true
+}
+// mock for $goRouter
+config.mocks['$goRouter'] = (path) => {
+  return path
+}
 
 Vue.use(Vuetify);
 
